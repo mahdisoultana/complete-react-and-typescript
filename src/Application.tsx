31f42ac71f88e6eae94efe3c8e2df949c1f34af6
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2023-01-27 20:36:26 +0100

    useProduct hook for useReducer with typescript

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,10 +1,12 @@
 import Home from './home/Home';
 import Greating from './lesssons/greeting/Greeting';
+import { CardProducts } from './lesssons/state-reduer/ButtonReducer';
 
 function Application() {
   return (
     <Home>
       <Greating name="Mahdi SOULTANA" />
+      <CardProducts />
     </Home>
   );
 }
--- src/lesssons/state-reduer/ButtonReducer.tsx
+++ src/lesssons/state-reduer/ButtonReducer.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useReducer } from 'react';
 
 type CategoryType = 'water' | 'stone' | 'metal' | 'gaz';
 
@@ -9,25 +9,134 @@ interface Product {
   price: number;
 }
 
-interface useProductState {
+interface ProductState {
   products: Product[];
+}
+interface utilits {
   addItem: (product: Product) => void;
   removeItem: (id: string) => void;
+  setProduct: (products: Product[]) => void;
 }
 
-const addItem: useProductState['addItem'] = (product: Product) =>
-  console.log('item added');
-const removeItem: useProductState['removeItem'] = (id: string) =>
-  console.log('item added');
+const intialState: ProductState = {
+  products: [],
+};
+interface AddAction {
+  type: 'add';
+  product: Product;
+}
+interface RemoveAction {
+  type: 'remove';
+  id: string;
+}
+interface SetProductsAction {
+  type: 'set-products';
+  products: Product[];
+}
+type ActionType = AddAction | RemoveAction | SetProductsAction;
+// ----------------------------------------------------------------------
 
-function useProduct(): useProductState {
+function reducer(state: ProductState, action: ActionType): ProductState {
+  switch (action.type) {
+    case 'add': {
+      return { ...state, products: [...state.products, action.product] };
+    }
+    case 'remove': {
+      const products = state.products.filter((item) => item.id !== action.id);
+      console.log(state);
+      return {
+        ...state,
+        products,
+      };
+    }
+    case 'set-products': {
+      return {
+        ...state,
+        products: action.products,
+      };
+    }
+    default: {
+      const exhaustiveCheck: never = action;
+      throw new Error(`Unhandled action case: ${exhaustiveCheck}`);
+    }
+  }
+}
+// ----------------------------------------------------------------------
+
+function useProduct(): ProductState & utilits {
+  const [state, dispatch] = useReducer(reducer, intialState);
+
+  const addItem: utilits['addItem'] = (product: Product) =>
+    dispatch({ type: 'add', product });
+  const removeItem: utilits['removeItem'] = (id: string) =>
+    dispatch({ type: 'remove', id });
+  const setProduct: utilits['setProduct'] = (products: Product[]) =>
+    dispatch({ type: 'set-products', products });
+
   return {
     addItem,
     removeItem,
-    products: [{ id: '1', name: 'product 1', category: 'stone', price: 34 }],
+    products: state.products,
+    setProduct,
   };
 }
+// ----------------------------------------------------------------------
 
-function AddItem(): ReactElement {
-  return <button>Add into Cart</button>;
+function AddRemoveItemButton({ product }: { product: Product }): ReactElement {
+  const { addItem, removeItem } = useProduct();
+  return (
+    <>
+      <button onClick={() => addItem(product)}>Add {product.name}</button>
+      <button
+        onClick={() => {
+          console.log('remove');
+          removeItem(product.id);
+        }}
+      >
+        remove {product.name}
+      </button>
+    </>
+  );
+}
+// ----------------------------------------------------------------------
+//root point
+export function CardProducts(): ReactElement {
+  const { products, setProduct } = useProduct();
+
+  useEffect(() => {
+    getProducts().then((products) => {
+      setProduct(products);
+    });
+  }, []);
+  console.log({ products });
+  return (
+    <>
+      {products.map((item) => (
+        <CardProduct key={item.id} product={item} />
+      ))}
+    </>
+  );
+}
+// ----------------------------------------------------------------------
+function CardProduct({ product }: { product: Product }) {
+  return (
+    <div key={product.id}>
+      <p>{product.name}</p>
+      <p>{product.price}</p>
+      <p>{product.category}</p>
+      <AddRemoveItemButton product={product} />
+    </div>
+  );
+}
+function getProducts(): Promise<Product[]> {
+  return Promise.resolve([
+    { id: '1', name: 'product-1', price: 12, category: 'stone' },
+  ]);
+}
+// exaustive Check
+// Externally-visible signature
+function exaustiveCheck(action: never): never;
+// Implementation signature
+function exaustiveCheck(action: ActionType) {
+  throw new Error('Unknown action : ' + action.type);
 }
